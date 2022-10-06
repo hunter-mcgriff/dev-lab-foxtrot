f60b5d1c637a2f67a168632e0328867cf13cfbac
hunter-mcgriff <111534236+hunter-mcgriff@users.noreply.github.com>
2022-10-06 14:55:25 -0500

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+const info = ["hmcgriff@una.edu", "CIS-376", "University of North Alabama"];
+
+
+
+for (j = 0; j < info.length; j++) {
+    //do something
+    $("#arrayOutput").append(info[j] + "<br/>");
+  }
